a20c9a78da3d285088b6fc9d821514b5fe789a03
JiayinnLiu <44830133+JiayinnLiu@users.noreply.github.com>
2023-08-13 23:01:38 -0700

    Added all frontend and backend functions

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +14,25 @@ function Login() {
   })
 
   const {email, password} = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, messsage} = useSelector(
+  (state) => state.auth)
+
+  useEffect( () => {
+    if(isError) {
+      toast.error(messsage)
+    }
+
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, messsage, navigate, dispatch])
+
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -18,14 +43,26 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
   }
 
-  return <>
+  if(isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
     <section className='heading'>
       <h1>
         <FaSignInAlt /> Login
       </h1>
-      <p>Login and start setting goals</p>
+      <p>Login and start</p>
     </section>
 
     <section className='form'>
@@ -64,6 +101,7 @@ function Login() {
       </form>
     </section>
   </>
+  )
 }
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import {FaUser} from 'react-icons/fa'
+import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -11,6 +16,24 @@ function Register() {
 
   const {name, email, password, password2} = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, messsage} = useSelector(
+  (state) => state.auth)
+
+  useEffect( () => {
+    if(isError) {
+      toast.error(messsage)
+    }
+
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, messsage, navigate, dispatch])
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -20,9 +43,25 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    if(password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
+    }
   }
 
-  return <>
+  if (isLoading) {
+    return <Spinner />
+  }
+  return (
+  <>
     <section className='heading'>
       <h1>
         <FaUser /> Register
@@ -84,6 +123,7 @@ function Register() {
       </form>
     </section>
   </>
+  )
 }
 
 export default Register
